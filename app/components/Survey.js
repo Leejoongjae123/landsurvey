91b7a46ecdd419744daf0706a333d3cf8d9315f7
ljj33 <ljj3347@naver.com>
2024-05-28 09:28:43 +0900

    240528

--- app/components/Survey.js
+++ app/components/Survey.js
@@ -4,25 +4,26 @@ import { Button } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
 import { Checkbox } from "@nextui-org/react";
 import { DatePicker } from "@nextui-org/react";
+import { Slider } from "@nextui-org/react";
 
 function Survey() {
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Tell me about your real estate needs
         </h2>
-        <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
           Got a technical issue? Want to send feedback about a beta feature?
           Need details about our Business plan? Let us know.
         </p>
-        <form action="#" class="space-y-8">
+        <form action="#" className="space-y-8">
           <div className="flex flex-col w-full flex-wrap md:flex-nowrap gap-4">
-            <div>❓ what's your name?</div>
+            <div>❓ 이름을 알려주세요</div>
             <Input type="email" label="Email" />
           </div>
           <div className="flex flex-col w-full flex-wrap md:flex-nowrap gap-4">
-            <div>❓ what's your name?</div>
+            <div>❓ 사이즈를 알려주세요</div>
             <div className="flex gap-4 justify-between">
               <Checkbox defaultSelected size="md">
                 Medium
@@ -36,9 +37,23 @@ function Survey() {
             </div>
           </div>
           <div className="flex flex-col w-full flex-wrap md:flex-nowrap gap-4">
-            <div>❓ What's your name?</div>
+            <div>❓ 날짜를 알려주세요</div>
             <div className="flex w-full ">
               <DatePicker label="Birth date" className="w-1/3" />
+            </div>
+          </div>
+          <div className="flex flex-col w-full flex-wrap md:flex-nowrap gap-4">
+            <div>❓ 원하시는 금액대를 알려주세요</div>
+            <div className="flex w-full ">
+              <Slider
+                label="Price Range"
+                step={50}
+                minValue={0}
+                maxValue={1000}
+                defaultValue={[100, 500]}
+                formatOptions={{ style: "currency", currency: "USD" }}
+                className="max-w-md"
+              />
             </div>
           </div>
 
