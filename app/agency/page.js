45611a12ddc824b9776e6746c74fa974979f987c
ljj33 <ljj3347@naver.com>
2024-05-31 01:26:18 +0900

    240531_5

--- app/agency/page.js
+++ app/agency/page.js
@@ -152,7 +152,7 @@ function page() {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', 'qr_code.png');
+      link.setAttribute('download', `qr_code_${getCurrentTimeString()}.png`);
       document.body.appendChild(link);
       link.click();
       link.remove();
